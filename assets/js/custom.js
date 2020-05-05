db3f8243e11951a2b75b67f3dbfc31031c7361a8
camilo <camilofosco@gmail.com>
2020-05-04 23:29:41 -0400

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -59,9 +59,8 @@ var custom = {
                   {
                       var text = rawFile.responseText;
                       var allLines = text.split(/\r\n|\n/);
-                      allLines = allLines.filter(function (el) {
-                                                return el != null;
-                                              });
+                      print()
+                      allLines = allLines.filter(Boolean);
                       console.log('USING FOLD:', gup('url'))
                       console.log(allLines)
                       num_imgs_to_label = allLines.length
